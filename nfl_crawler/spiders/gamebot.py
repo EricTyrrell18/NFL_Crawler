700fe0dd4d2910e6aca92201f0c9c6b38493636f
Eric Tyrrell <cicero222@Ignoramus.local>
2019-10-28 13:56:32 -0400

    Clean up gamebot.py
    - Reduce redundant code by creating create_game_items()

--- nfl_crawler/spiders/gamebot.py
+++ nfl_crawler/spiders/gamebot.py
@@ -15,55 +15,18 @@ class GamebotSpider(scrapy.Spider):
         # go through all the combinations
         for comb in product(years,games):
             yield scrapy.Request(base_url.format(comb[0], comb[1]), callback=self.parse) 
-
-    def parse(self, response):
-
-        # This isn't gonna be pretty, but it seemingly worked
-        # Grab the days games are played on.
-        days = response.xpath('//li[@class="schedules-list-date"]/span/span/text()').extract()
-        logging.debug(days)
-        # recursively grab data between the bounds of the first date and the last date
-        while len(days) >= 2:
-            # Most of the game data's between two dates.
-            # The date's are also at the same level as the games, so it's hard to use pure css to grab the info correctly
-            # think of it as before this date and after this other date 
-            xpath_query = '//li[following-sibling::li[.//span/text()="{}"]][preceding-sibling::li[.//span/text()="{}"]]'.format(days[1], days[0])
-
-            teams = response.xpath(xpath_query)
-
-            # TODO: make a function to remove redundancy
-            away_teams = teams.css("span.team-name.away::text").extract()
-            home_teams = teams.css("span.team-name.home::text").extract()
-            away_scores = teams.css("span.team-score.away::text").extract()
-            home_scores = teams.css("span.team-score.home::text").extract()
-            stadium = teams.xpath('./div/@data-site').extract()
-
-            game_stats  = list(zip(away_teams, home_teams, away_scores, home_scores, stadium))
-
-            for game in game_stats:
-                  item =  NflGameItem()
-                  item["away_team"] = game[0]
-                  item["home_team"] = game[1]
-                  item["away_score"] = game[2]
-                  item["home_score"] = game[3]
-                  item["stadium"] = game[4]
-                  item["date"] = days[0]
-                  item["year"] = response.url.split("/")[4]
-                  yield item
-            # move on to the next day
-            days = days[1:]
-
-        # still a single day left, but it's an easy cleanup
-        # TODO: make a function to remove redundancy
-        teams = response.xpath('//li[preceding-sibling::li[.//span/text()="{}"]]'.format(days[0]))
+    def create_game_items(self, teams, year, date):
+        #logging.debug("starting create_game_items")                
         away_teams = teams.css("span.team-name.away::text").extract()
         home_teams = teams.css("span.team-name.home::text").extract()
         away_scores = teams.css("span.team-score.away::text").extract()
         home_scores = teams.css("span.team-score.home::text").extract()
         stadium = teams.xpath('./div/@data-site').extract()
-        
+        #logging.debug(away_teams)
+        #logging.debug("zipping stats")
+
         game_stats  = list(zip(away_teams, home_teams, away_scores, home_scores, stadium))
-        logging.debug(game_stats)
+
         for game in game_stats:
             item =  NflGameItem()
             item["away_team"] = game[0]
@@ -71,7 +34,37 @@ class GamebotSpider(scrapy.Spider):
             item["away_score"] = game[2]
             item["home_score"] = game[3]
             item["stadium"] = game[4]
-            item["date"] = days[0]
-            item["year"] = response.url.split("/")[4]
+            item["year"] = year
+            item["date"] = date
             yield item
+        #logging.debug("finished create_game_items")
+
+    def parse(self, response):
+        #logging.debug("begin parse")
+        # This isn't gonna be pretty, but it seemingly worked
+        # Grab the days games are played on.
+        days = response.xpath('//li[@class="schedules-list-date"]/span/span/text()').extract()
+        year = response.url.split("/")[4]
+        #logging.debug(days)
+        #logging.debug(year)
+
+        # Iteratively grab data between the days[0] and days[1] and the delete days[0]
+        while len(days) >= 2:
+            # Most of the game data's between two dates.
+            # The date's are also at the same level as the games, so it's hard to use pure css to grab the info correctly
+            # think of it as before this date and after this other date 
+            xpath_query = '//li[following-sibling::li[.//span/text()="{}"]][preceding-sibling::li[.//span/text()="{}"]]'.format(days[1], days[0])
+
+            teams = response.xpath(xpath_query)
+            #logging.debug("Entering create_game_items")
+            for game in self.create_game_items(teams, year, days[0]):
+                yield game
+
+            # move on to the next day
+            days = days[1:]
+
+        # Finish scraping the last day of games
+        teams = response.xpath('//li[preceding-sibling::li[.//span/text()="{}"]]'.format(days[0]))
+        for game in self.create_game_items(teams, year, days[0]):
+            yield game
         
